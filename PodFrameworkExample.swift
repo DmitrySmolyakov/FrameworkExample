2a2c41d7667f9cd7ca4735277a5aea3e8639f598
Dmitry Smolyakov <dmitrysmolyakov@Vasilys-Mini.elilink.by>
2017-03-10 11:32:13 +0300

    initial commit

---
+++ PodFrameworkExample.swift
@@ -0,0 +1,16 @@
+//
+//  PodFrameworkExample.swift
+//  PodFrameworkExample
+//
+//  Created by Dmitry Smolyakov on 3/10/17.
+//  Copyright © 2017 Dmitry Smolyakov. All rights reserved.
+//
+
+import Foundation
+
+
+public class PodFrameworkExample: NSObject {
+    public static func printString(string: String) {
+        print(string)
+    }
+}
